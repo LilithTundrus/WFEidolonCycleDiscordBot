2a912dc2ffb12b04725e10756a5827a2adaab7c0
Lilith Tundrus <echofox123@gmail.com>
2018-07-09 21:08:11 -0400

    Conversion was successful

--- bot.js
+++ bot.js
@@ -21,35 +21,45 @@ client.login(config.token);
 client.on('ready', () => {
     console.log(`Connected to Discord.\nLogged in as ${client.user.username} (${client.user.id})`);
 
+    // Set the bot to be 'playing' a game
     client.user.setActivity(`Warframe (Cetus - Level 10-30)`);
 });
 
 
-bot.on('message', function(user, userID, channelID, message, evt) {
-    if (message.substring(0, 1) == '~') {                           // listen for messages that will start with `~`
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-        // log any messages sent to the bot for debugging
-        fs.appendFileSync('discordMessagelog.log', `${user} sent: ${message} at ${Date.now()}`);
-        console.log(`${user} sent: ${message} at ${Date.now()}`);
-        args = args.splice(1);
-        switch (cmd) {                                              // bot needs to know if it will execute a command
-            case 'help':                                            // display the help file
-                let helpMsg = fs.readFileSync('./helpNotes.txt');
-                bot.sendMessage({
-                    to: channelID,
-                    message: '```' + helpMsg.toString() + '```'     // the ``` is there so discord treats it as monospace
-                });
-                break;
-            case 'ver':
-                bot.sendMessage({
-                    to: channelID,
-                    message: `Version: ${ver} Running on server: ${os.type()} ${os.hostname()} ${os.platform()} ${os.cpus()[0].model}`
-                });
-                break;
-            case 'time':
-                return getTime(channelID);
-        }
+client.on('message', async message => {
+    let prefix = '~';
+    // TODO: handle mentions and help messages
+    // This event will run on every single message received, from any channel or DM
+
+    // It's good practice to ignore other bots. This also makes your bot ignore itself
+    if (message.author.bot) return;
+    // Also good practice to ignore any message that does not start with the bot'ss prefix, 
+    if (message.content.indexOf(prefix) !== 0) return;
+
+    // Here we separate the 'command' name, and the 'arguments' for the command.
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    switch (command) {
+        case 'help':                                            // display the help file
+            let helpMsg = fs.readFileSync('./helpNotes.txt');
+            message.channel.send('```' + helpMsg.toString() + '```');
+            break;
+        case 'ver':
+            message.channel.send(`Version: ${ver} Running on server: ${os.type()} ${os.hostname()} ${os.platform()} ${os.cpus()[0].model}`);
+            break;
+        case 'ping':
+            // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+            // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+            const m = await message.channel.send('Ping?');
+            m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+            break;
+        case 'time':
+            return getTime(message.channel.id);
+        default:
+            // If command character + unknown command is given we at least need to let the user know
+            let errorEmbed = `Uknown command: **${command}**`;
+            return message.channel.send(errorEmbed);
     }
 });
 
@@ -57,10 +67,7 @@ bot.on('message', function(user, userID, channelID, message, evt) {
 function getTime(channelIDArg) {
     parser.updateTime()
         .then((response) => {
-            bot.sendMessage({
-                to: channelIDArg,
-                message: response
-            });
+            let discordChannel = client.channels.get(channelIDArg);
+            discordChannel.send(response);
         })
-
 }
